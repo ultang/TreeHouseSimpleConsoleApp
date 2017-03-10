bc39a1c78f47f6b4344db1600fc07de1a1eda996
Ultan Gannon <Ultan Gannon>
2017-03-10 13:39:26 +0000

    Add project files.

---
+++ TreeHouseSimpleConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeHouseSimpleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a comment
+            string greeting = "Hello World";
+
+            string name = "Ultan Gannon";
+        }
+
+        static string[] SplitName(string name)
+        {
+            return name.Split('');
+        }
+
+    }
+}
